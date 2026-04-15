e70f89f32bd9ae9ad809d5d6632631b75a7d3350
erick6aelgg <erick6aelgg@ciencias.unam.mx>
2026-04-15 07:43:28 -0600

    feat: Views. Métodos implementados junto a xml necesarios.

--- app/src/main/java/com/example/myapplication/view/PreferencesFragment.java
+++ app/src/main/java/com/example/myapplication/view/PreferencesFragment.java
@@ -1,4 +1,15 @@
 package com.example.myapplication.view;
 
-public class PreferencesFragment {
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.example.myapplication.R;
+
+public class PreferencesFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        // Cargamos las preferencias desde el recurso XML
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
 }
